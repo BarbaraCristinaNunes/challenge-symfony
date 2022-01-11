2b5570c8a63c01ef62850b50ddfb94837e999608
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-11 14:06:56 +0100

    created method changeMyName()

--- symfony-mcv/src/Controller/LearningController.php
+++ symfony-mcv/src/Controller/LearningController.php
@@ -5,9 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class LearningController extends AbstractController
 {
+
     #[Route('/learning', name: 'learning')]
     public function index(): Response
     {
@@ -17,7 +21,7 @@ class LearningController extends AbstractController
 
 
     }
-    #[Route('/about-me', name: 'homepage')]
+    #[Route('/about', name: 'about')]
     public function aboutMe(): Response
     {
         return $this->render('learning/index.html.twig', [
@@ -27,17 +31,26 @@ class LearningController extends AbstractController
 
     }
     #[Route('/homepage', name: 'homepage')]
-    public function showName(): Response
+    public function showName(Session $session): Response
     {
-        // if(isset($_POST['btn']) && isset($_POST['name'])){
-        //     $name = $_POST['name'];
-        // }else{
+        if($session->get('name')){
+            $name = $session->get('name');
+            $test = 'deu certo';
+        }else{
             $name = "Unknown";
-        // }
+            $test = 'ola';
+        }        
         return $this->render('learning/showMyName.html.twig', [
             'name' => $name,
+            'test' => $test
         ]);
 
-
+    }
+    #[Route('/changeMyName', name: 'changeMyName', methods: ['POST'])]
+    public function changeMyName(Request $request, Session $session): RedirectResponse
+    {
+        $name = $request->request->get('name');
+        $session->set('name', $name);
+        return $this->redirectToRoute('homepage');
     }
 }
