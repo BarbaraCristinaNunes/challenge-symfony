525d51eae17e25aafcffed5499b47f597e9a1b1a
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-12 16:18:09 +0100

    Update Logger.php

--- dependency-injection/src/Service/Logger.php
+++ dependency-injection/src/Service/Logger.php
@@ -5,7 +5,7 @@ class Logger
 {
     public function logMessage(string $message){
         $log = "";
-        $log .= $message. "<br>";
-        file_put_contents("log.info",$log);
+        $log .= $message. "\n";
+        file_put_contents("log.info",$log, FILE_APPEND);
     }    
 }
