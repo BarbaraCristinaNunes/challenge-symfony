cdcbe2edffa1f0d33a2870542bf462d066f5891a
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-13 15:10:32 +0100

    now I can show date

--- symfony-mcv/src/Controller/LearningController.php
+++ symfony-mcv/src/Controller/LearningController.php
@@ -8,7 +8,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Time;
 
 class LearningController extends AbstractController
 {
